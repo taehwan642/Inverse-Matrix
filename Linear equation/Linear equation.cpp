2a112c82691dc02ebd55d5794176488cad5de152
Taehwan Kim <taehwan642@naver.com>
2020-03-28 03:39:57 +0900

    INVERSE UPDATE

--- Linear equation/Linear equation.cpp
+++ Linear equation/Linear equation.cpp
@@ -71,6 +71,30 @@ public:
 		cout << endl;
 		cout << endl;
 	}
+	Matrix* operator*(float a)
+	{
+		Matrix* r = new Matrix(maxcol, maxrow);
+		for (int i = 0; i < maxcol; i++)
+		{
+			for (int j = 0; j < maxrow; j++)
+			{
+				r->matrixdata[i][j] = matrixdata[i][j] * a;
+			}
+		}
+		return r;
+	}
+	Matrix* operator=(Matrix* m)
+	{
+		Matrix* r = new Matrix(maxcol, maxrow);
+		for (int i = 0; i < maxcol; i++)
+		{
+			for (int j = 0; j < maxrow; j++)
+			{
+				r->matrixdata[i][j] = m->matrixdata[i][j];
+			}
+		}
+		return r;
+	}
 };
 struct Manager
 {
@@ -124,29 +148,119 @@ struct Manager
 
 		return out;
 	}
+	void getCofactor(Matrix* A, Matrix* temp, int p, int q, int n)
+	{
+		int i = 0, j = 0;
+
+		for (int row = 0; row < n; row++)
+		{
+			for (int col = 0; col < n; col++)
+			{
+				if (row != p && col != q)
+				{
+					temp->matrixdata[i][j++] = A->matrixdata[row][col];
+					if (j == n - 1)
+					{
+						j = 0;
+						i++;
+					}
+				}
+			}
+		}
+	}
+	int determ(Matrix* a, int n) 
+	{
+		int det = 0, p, h, k, i, j;
+		Matrix* temp = new Matrix(n, n);
+		if (n == 1) 
+		{
+			return a->matrixdata[0][0];
+		}
+		else if (n == 2) 
+		{
+			det = (a->matrixdata[0][0] * a->matrixdata[1][1] - a->matrixdata[0][1] * a->matrixdata[1][0]);
+			return det;
+		}
+		else 
+		{
+			for (p = 0; p < n; p++) 
+			{
+				h = 0;
+				k = 0;
+				for (i = 1; i < n; i++) 
+				{
+					for (j = 0; j < n; j++) 
+					{
+						if (j == p) 
+						{
+							continue;
+						}
+						temp->matrixdata[h][k] = a->matrixdata[i][j];
+						k++;
+						if (k == n - 1) 
+						{
+							h++;
+							k = 0;
+						}
+					}
+				}
+				det = det + a->matrixdata[0][p] * pow(-1, p) * determ(temp, n - 1);
+			}
+			return det;
+		}
+	}
+
+	void adjoint(Matrix* A, Matrix* adj)
+	{
+		// temp is used to store cofactors of A[][] 
+		int sign = 1;
+		Matrix* temp = new Matrix(A->maxcol, A->maxrow);
+
+		for (int i = 0; i < A->maxcol; i++)
+		{
+			for (int j = 0; j < A->maxrow; j++)
+			{
+				// Get cofactor of A[i][j] 
+				getCofactor(A, temp, i, j, A->maxcol);
+
+				// sign of adj[j][i] positive if sum of row 
+				// and column indexes is even. 
+				sign = ((i + j) % 2 == 0) ? 1 : -1;
+
+				// Interchanging rows and columns to get the 
+				// transpose of the cofactor matrix 
+				adj->matrixdata[i][j] = (sign) * (determ(temp, A->maxcol - 1));
+			}
+		}
+	}
+	Matrix* Linearequation()
+	{
+		//for i = x,y,z 순
+	}
 };
-int main(void)
+
+void MatrixMultiplyprogram()
 {
 	Manager MNG;
 	//입력값 받아와야함
-	cout << "★행렬의 곱셈 프로그램★" << endl << endl;
+	//cout << "★행렬의 곱셈 프로그램★" << endl << endl;
 
-	cout << "★행렬이 자동으로 2개 만들어질거라구~!★" << endl << endl;
-	cout << "★1번째 행렬의 행의 크기를 입력하라구~!★" << endl;
+	//cout << "★행렬이 자동으로 2개 만들어질거라구~!★" << endl << endl;
+	//cout << "★1번째 행렬의 행의 크기를 입력하라구~!★" << endl;
 
 	int hang = 0, yull = 0;
 
 	cin >> yull;
-	cout << "★1번째 행렬의 열의 크기를 입력하라구~!★" << endl;
+	//cout << "★1번째 행렬의 열의 크기를 입력하라구~!★" << endl;
 
 	cin >> hang;
-	
+
 	Matrix* matrix1 = new Matrix(yull, hang);
 
-	cout << "★행렬의 원소를 1,1 부터 1,2 순서대로 너가 입력한 크기만큼까지 입력할거라구~!★" << endl;
+	//cout << "★행렬의 원소를 1,1 부터 1,2 순서대로 너가 입력한 크기만큼까지 입력할거라구~!★" << endl;
 	int inputdata = 0;
-	
-	for (int i = 0; i < yull ; i++)
+
+	for (int i = 0; i < yull; i++)
 	{
 		for (int j = 0; j < hang; j++)
 		{
@@ -156,41 +270,70 @@ int main(void)
 			matrix1->OuptPutAll();
 		}
 	}
-	cout << "★다 했니? 이제 2번째 행렬의 행을 넣으라구~★" << endl;
+	float det = (float)MNG.determ(matrix1, yull);
 
-	int hang2 = 0, yull2 = 0;
-	cin >> yull2;
-	cout << "★이제 2번째 행렬의 열을 넣으라구~★" << endl;
-	cin >> hang2;
-	Matrix* matrix2 = new Matrix(yull2, hang2);
+	MNG.MatrixTranspos(matrix1);
+	//matrix1->OuptPutAll();
 
-	cout << "★행렬의 원소를 1,1 부터 1,2 순서대로 너가 입력한 크기만큼까지 입력할거라구~!★" << endl;
+	Matrix* adj = new Matrix(matrix1->maxcol, matrix1->maxrow);
+	MNG.adjoint(matrix1, adj);
+	//adj->OuptPutAll();
 
-
-	int inputdata2 = 0;
-	for (int i = 0; i < yull2; i++)
+	Matrix* r = new Matrix(matrix1->maxcol, matrix1->maxrow);
+	
+	//r = adj * (1 / det);
+	for (int i = 0; i < adj->maxcol; i++)
 	{
-		for (int j = 0; j < hang2; j++)
+		for (int j = 0; j < adj->maxrow; j++)
 		{
-			cin >> inputdata2;
-			matrix2->InputMatrix(i, j, inputdata2);
-			cout << endl;
-			matrix2->OuptPutAll();
+			adj->matrixdata[i][j] *= (1 / det);
 		}
 	}
+	r = adj;
+	r->OuptPutAll();
+	//cout << "★다 했니? 이제 2번째 행렬의 행을 넣으라구~★" << endl;
 
-	cout << endl << endl;
+	//int hang2 = 0, yull2 = 0;
+	//cin >> yull2;
+	//cout << "★이제 2번째 행렬의 열을 넣으라구~★" << endl;
+	//cin >> hang2;
+	//Matrix* matrix2 = new Matrix(yull2, hang2);
 
-	cout << "★두 행렬의 곱한 값은!★" << endl << endl;
+	//cout << "★행렬의 원소를 1,1 부터 1,2 순서대로 너가 입력한 크기만큼까지 입력할거라구~!★" << endl;
 
-	Matrix* result = new Matrix(yull2 , hang);
+
+	//int inputdata2 = 0;
+	//for (int i = 0; i < yull2; i++)
+	//{
+	//	for (int j = 0; j < hang2; j++)
+	//	{
+	//		cin >> inputdata2;
+	//		matrix2->InputMatrix(i, j, inputdata2);
+	//		cout << endl;
+	//		matrix2->OuptPutAll();
+	//	}
+	//}
+
+	//cout << endl << endl;
+
+	//cout << "★두 행렬의 곱한 값은!★" << endl << endl;
+
+	/*Matrix* result = new Matrix(yull2, hang);
 
 	result = MNG.MatrixMultiply(result, matrix1, matrix2);
 
-	result->OuptPutAll();
+	result->OuptPutAll();*/
+
+	//이제 역행렬 조져야징?
+
 	int a = 0;
 	for (int i = 0; i < 1; i++)
 	{
 		cin >> a;
 	}
+}
+
+int main(void)
+{
+	MatrixMultiplyprogram();
 }
